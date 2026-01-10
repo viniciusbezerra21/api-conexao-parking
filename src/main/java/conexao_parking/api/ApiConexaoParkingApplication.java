ac1cea117d2358f1456ee396dc403e3173134128
Vinicius Bezerra <viniciusg21bezerra@gmail.com>
2026-01-10 10:27:40 -0300

    Initial commit

---
+++ src/main/java/conexao_parking/api/ApiConexaoParkingApplication.java
@@ -0,0 +1,13 @@
+package conexao_parking.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConexaoParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConexaoParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/conexao_parking/api/ApiConexaoParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package conexao_parking.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConexaoParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
