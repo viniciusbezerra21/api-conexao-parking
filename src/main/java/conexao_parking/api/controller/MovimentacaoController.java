24e92b09b323f0f6706bd496f3a7ea1a71fb5710
Vinicius.Bezerra <viniciusg21bezerra@gmail.com>
2026-01-16 09:37:33 -0300

    listagem de movimentacao adicionada

--- src/main/java/conexao_parking/api/controller/MovimentacaoController.java
+++ src/main/java/conexao_parking/api/controller/MovimentacaoController.java
@@ -1,10 +1,15 @@
 package conexao_parking.api.controller;
 
 import conexao_parking.api.movimentacao.DadosCadastroMovimentacao;
+import conexao_parking.api.movimentacao.DadosListagemMovimentacao;
 import conexao_parking.api.movimentacao.MoviementacaoService;
-import conexao_parking.api.movimentacao.Movimentacao;
+import conexao_parking.api.movimentacao.MovimentacaoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 public class MovimentacaoController {
 
     private final MoviementacaoService service;
+    @Autowired
+    private MovimentacaoRepository repository;
 
     public MovimentacaoController(MoviementacaoService service) {
         this.service = service;
@@ -23,6 +30,9 @@ public class MovimentacaoController {
         service.cadastrar(dados);
     }
 
-
+    @GetMapping
+    public Page<DadosListagemMovimentacao> listar(@PageableDefault(size = 10)Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMovimentacao::new);
+    }
 
 }
