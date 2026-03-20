6eadb8eb3ba131d0601660fbc1c7a393151a380c
Vinicius.Bezerra <viniciusg21bezerra@gmail.com>
2026-03-20 08:39:45 -0300

    Ajustando enum pra receber os formatos de Jackson enviados para o typeScript

--- src/main/java/conexao_parking/api/domain/veiculo/TipoVeiculo.java
+++ src/main/java/conexao_parking/api/domain/veiculo/TipoVeiculo.java
@@ -1,7 +1,12 @@
 package conexao_parking.api.domain.veiculo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum TipoVeiculo {
+    @JsonProperty("Carro")
     CARRO,
+    @JsonProperty("Moto")
     MOTO,
+    @JsonProperty("Caminhão")
     CAMINHAO,
 }
