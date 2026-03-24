0e1837612195497a4e285b64e56445bd6b225156
Vinicius Bezerra <viniciusg21bezerra@gmail.com>
2026-03-24 15:46:49 -0300

    Liberando cors para consumir a api

--- src/main/java/conexao_parking/api/infra/security/SecurityConfigurations.java
+++ src/main/java/conexao_parking/api/infra/security/SecurityConfigurations.java
@@ -54,7 +54,7 @@ public class SecurityConfigurations {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200" , "https://conexao-parking.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
